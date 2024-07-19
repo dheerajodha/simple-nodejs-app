4f5b72fffd9756c686941ed3ca882bd0d6c6484f
Pat O'Connor <114754356+chipspeak@users.noreply.github.com>
2024-07-19 12:33:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
 
+// adding to trigger
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(3000, () => console.log('Server ready'))

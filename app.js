abb12653fa56d5994070010417a496b02bd7561e
Pat O'Connor <paoconno@redhat.com>
2024-08-20 12:11:44 +0100

    testing change - should fail
    Signed-off-by: Pat O'Connor <paoconno@redhat.com>

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 
 // test
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Tech Meetup!'))
 app.listen(3000, () => console.log('Server ready'))

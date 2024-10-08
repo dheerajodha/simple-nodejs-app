80ce247e15c3c6c26f2c4193ab3d116edade4bb0
Pat O'Connor <paoconno@redhat.com>
2024-10-08 11:36:33 +0100

    testing for STONEINTG-851
    Signed-off-by: Pat O'Connor <paoconno@redhat.com>

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 
 // test
-app.get('/', (req, res) => res.send('Hello Tech Meetup!'))
+app.get('/', (req, res) => res.send('Hello!!'))
 app.listen(3000, () => console.log('Server ready'))
